1e4dbcc352ec97fdac1f322155cc6645b307fb3f
N4turerl <73256394+N4turerl@users.noreply.github.com>
2021-01-12 18:39:32 -0500

    Add files via upload

---
+++ Homework Lesson 2 Exercise 3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+name = input('what is your name?: ')
+
+if name == 'joe':
+    print('your name is joe')
+else:
+    print('your name is '+name)
+
